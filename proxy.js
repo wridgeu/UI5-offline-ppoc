e68b5c4b7cb8b249a7f6c2a23a5131a2028dd444
Marco Beier <marco-beier@t-online.de>
2019-09-24 23:29:03 +0200

    Added cors-anywhere package; Added proxy.js that uses cors-anywhere; Updated package.json's start-script

--- proxy.js
+++ proxy.js
@@ -1,12 +1,13 @@
 var cors_proxy = require('cors-anywhere');
-var host = "0.0.0.0";
-var port = 8080;
+var host = process.env.HOST || '0.0.0.0';
+var port = process.env.PORT || 8080;
 
+
+// should make requests usable if you add your http://localhost:<port>/<actual_URL> in front of stuff
 cors_proxy.createServer({
     originWhitelist: [],
     requireHeader: ['origin', 'x-requested-with' ],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function(){
-    //console.log("Cors is running on" + host + ":" + port);
-    console.log(`Cors is running on ${host} and ${port}`);
+    console.log(`Cors is listening on ${host} and ${port}`);
 })
