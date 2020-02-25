68b285942f158551a1ba9ac0393499d4271b205e
Marco Beier <marco-beier@t-online.de>
2020-02-25 23:11:27 +0100

    Adjusted a few UI Parts for some dev-testing

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
 	"./BaseController",
-	"../model/formatter",	
+	"../model/formatter",
 	"sap/m/MessageToast"
 ], function (Controller, formatter, MessageToast) {
 	"use strict";
@@ -10,7 +10,7 @@ sap.ui.define([
 		formatter: formatter,
 
 		onInit: function () {
-			
+
 		},
 		onAddObject: function () {
 			//Testdata
@@ -29,14 +29,12 @@ sap.ui.define([
 			//clear values again - TODO
 			this.byId("inputNLPLA").setValue("");
 			this.byId("inputNLPLA").getValue("");
-
 			this.indexedDB.addOjectToDatabase('keyvaluepairs', owht);
 			console.log(owht.TANUM + " got added!");
 		},
 		onRead: function () {
 			//Read from ObjectStore: DB, OStore, Readoption:READONLY
-			var val = this.indexedDB.readAllFromDatabase('keyvaluepairs');
-			console.log(val);
+			this.indexedDB.readAllFromDatabase('keyvaluepairs');
 		},
 		onDelete: function () {
 			// DBname, OStoreName, txoption, key from keypath
@@ -51,27 +49,27 @@ sap.ui.define([
 			//standard Version "1" ist, und es keine Version "1" mehr gibt
 		},
 		onUpdateOStore: function () {
-			//Testdata
-			var owht = {
-				TANUM: '',
-				VLPLA: '',
-				NLPLA: ''
-			};
+			/*Testdata Object
+			TANUM: '',
+			VLPLA: '',
+			NLPLA: ''
+			*/
+			var owht = {};
 			owht.TANUM = this.byId("inputKey").getValue();
 			owht.VLPLA = this.byId("inputnewVLPLA").getValue();
 			owht.NLPLA = this.byId("inputnewNLPLA").getValue();
-			//clear values again - TODO
-			this.byId("inputnewVLPLA").getValue("");
+			this.byId("inputnewVLPLA").setValue("");
 			this.byId("inputnewNLPLA").setValue("");
-			this.byId("inputKey").getValue("");
+			this.byId("inputKey").setValue("");
 
 			this.indexedDB.updateObjectInDatabase('keyvaluepairs', owht);
 		},
-		onCreateNewTable: function (){	
+		onCreateNewTable: function () {
 			this.indexedDB.createObjectStore([['Test', 'TANUM'], ['Test123', ['TANUM', 'TANUM2']]]);
 		},
-		onGetByKey: function(){
-			this.indexedDB.readByKey("keyvaluepairs", "193254491")
+		onGetByKey: function () {
+			var getKey = this.byId("getKey").getValue();
+			this.indexedDB.readByKey("keyvaluepairs", getKey)
 		}
 		// onSync: function () {
 		// 	//Dirty get core, some UI stuff for syncing ...
@@ -81,13 +79,11 @@ sap.ui.define([
 		// 	// navigator.serviceWorker.ready.then(function(reg) {
 		// 	// 	return reg.sync.register('sync-something');
 		// 	// })
-			
+
 		// 	//WebRfC SAP System - EWM1: var  url = 'http://10.199.2.253:8000/sap/bc/webrfc?_FUNCTION=Z_MRB_UI5SYNC&_name=' + tanumPar;
-			
 		// 	//Placeholder API for testing: https://jsonplaceholder.typicode.com/todos/1
-			
 		// 	// sap@home http://vhcalnplci.dummy.nodomain:8000/sap/bc/webrfc?_FUNCTION=Z_UI5_SYNC&_name=test; !Successfully tested on deployment of the SAP System!
-			
+
 		// 	// sap@home - uploaded (and build) ui5 app for testing Link:
 		// 	// https://vhcalnplci.dummy.nodomain:44300/sap/bc/ui5_ui5/sap/z_ui5sync_app_1/index.html?sap-client=001&sap-ui-language=DE&sap-ui-xx-devmode=true
 		// 	(function () {
