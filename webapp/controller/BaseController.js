61244ce1f4e9760fd0527a13add81c5be0ebc51d
Marco Beier <marco-beier@t-online.de>
2020-02-25 23:11:41 +0100

    Added some comments ...

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/mvc/Controller",	
 	"../model/indexedDB",
 	"../model/formatter"
 ], function(Controller, indexedDB, formatter) {
@@ -10,9 +10,10 @@ sap.ui.define([
 		formatter: formatter,
 		//Crate Instance: DatabaseName, DatabaseVersion, ArrayObject[[TableName, 'value'], [TableName [Value1, Value2]]]
 		indexedDB: indexedDB.getInstance("localstorage", "1", [['keyvaluepairs', 'TANUM'], ['keyvaluepairs1', ['TANUM', 'TANUM2']]]),
-		
+				
 		onInit: function () {
-			//move iddb instance to component.js and get instance via ownercomponent here ..
+			//Do not get Instance via OwnerComponent in Base-Class
+			//We would have to call onInit from inheriting objects like "BaseController.prototype.onInit.apply(this);"
 		}
 
 	});
